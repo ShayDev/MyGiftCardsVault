60d6f0894bf7b1db663d497fc2a69793b6e1245a
Shay N <shay.nadav@google.com>
2026-04-08 11:45:28 +0300

    setting shows on cards only

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -2,15 +2,19 @@
 
 import Link from 'next/link'
 import { useAuth } from '@clerk/nextjs'
+import { usePathname } from 'next/navigation'
 import { useLanguageStore } from '../hooks/useLanguageStore'
 import { getT } from '../lib/i18n'
 
+const HIDDEN_PATHS = ['/sign-in', '/onboarding']
+
 export default function HeaderNav() {
   const { isSignedIn } = useAuth()
+  const pathname = usePathname()
   const locale = useLanguageStore((s) => s.locale)
   const t = getT(locale)
 
-  if (!isSignedIn) return null
+  if (!isSignedIn || HIDDEN_PATHS.some((p) => pathname.startsWith(p))) return null
 
   return (
     <Link
