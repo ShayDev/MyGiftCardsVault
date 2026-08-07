e592622ba518d9367b336a261cbbc2b54336b07f
Shay N <shay.nadav@google.com>
2026-08-07 22:59:33 +0300

    menifest 404 fix

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/api/clerkWebhook'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/api/clerkWebhook', '/manifest.json'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
